96627516b2fb433fe2e49102506dd6d97e054d19
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-07 04:16:55 -0500

    Added icons to MenuBar & added styled to MenuBar

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import styles from './menuBar.module.css';
+import appleIcon from '../../../assets/apple.png';
+import battery from '../../../assets/battery.svg';
+import wifi from '../../../assets/wifi.svg';
+import search from '../../../assets/search.svg';
 
 function MenuBar() {
+  const [current, setCurrent] = useState('');
+
   return (
     <div className={styles.main}>
-      <section>Left</section>
-      <section>Right</section>
+      <section className={styles.left}>
+        <img src={appleIcon} alt="apple" className={styles.apple} />
+        {current && <div>{current}</div>}
+        <div>File</div>
+        <div>Edit</div>
+        <div>View</div>
+        <div>Go</div>
+        <div>Window</div>
+        <div>Help</div>
+      </section>
+      <section className={styles.right}>
+        <img src={battery} alt="" />
+        <img src={wifi} alt="" />
+        <img src={search} alt="" />
+      </section>
     </div>
   );
 }
