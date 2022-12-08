1b51378a7e17781c63713b6117ea7c0461804a34
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-08 00:39:33 -0500

    Reduced redundant code for left menu bar icons

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -10,8 +10,6 @@ import wifiIconWhite from '../../../assets/wifiW.svg';
 import searchIcon from '../../../assets/search.svg';
 import searchIconWhite from '../../../assets/searchW.svg';
 
-const DAYLIST = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 function MenuBar() {
   const [date, setDate] = useState(new Date());
   setInterval(() => setDate(new Date()), 60000);
@@ -19,6 +17,27 @@ function MenuBar() {
   const hours = date.getHours();
   const minutes =
     date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const dayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  const rightIcons = [
+    {
+      id: 'battery',
+      icon: batteryIcon,
+      iconWhite: batteryIconWhite,
+    },
+    {
+      id: '',
+      icon: wifiIcon,
+      iconWhite: wifiIconWhite,
+    },
+    {
+      id: 'search',
+      icon: searchIcon,
+      iconWhite: searchIconWhite,
+    },
+  ];
+
+  const leftIcons = ['File', 'Edit', 'View', 'Go', 'Window', 'Help'];
 
   return (
     <div className={styles.main}>
@@ -30,49 +49,24 @@ function MenuBar() {
           imgClass="apple"
           imgId=""
         />
-        <button type="button" className={styles['left-child']}>
-          File
-        </button>
-        <button type="button" className={styles['left-child']}>
-          Edit
-        </button>
-        <button type="button" className={styles['left-child']}>
-          View
-        </button>
-        <button type="button" className={styles['left-child']}>
-          Go
-        </button>
-        <button type="button" className={styles['left-child']}>
-          Window
-        </button>
-        <button type="button" className={styles['left-child']}>
-          Help
-        </button>
+        {leftIcons.map((i) => (
+          <button type="button" className={styles['left-child']}>
+            {i}
+          </button>
+        ))}
       </section>
       <section className={styles.right}>
-        <MenuBarIcon
-          containerClass="right-icon-container"
-          icon={batteryIcon}
-          iconWhite={batteryIconWhite}
-          imgClass="right-child"
-          imgId="battery"
-        />
-        <MenuBarIcon
-          containerClass="right-icon-container"
-          icon={wifiIcon}
-          iconWhite={wifiIconWhite}
-          imgClass="right-child"
-          imgId=""
-        />
-        <MenuBarIcon
-          containerClass="right-icon-container"
-          icon={searchIcon}
-          iconWhite={searchIconWhite}
-          imgClass="right-child"
-          imgId="search"
-        />
+        {rightIcons.map((i) => (
+          <MenuBarIcon
+            containerClass="right-icon-container"
+            icon={i.icon}
+            iconWhite={i.iconWhite}
+            imgClass="right-child"
+            imgId={i.id}
+          />
+        ))}
         <button type="button" className={styles['date-btn']}>{`${
-          DAYLIST[day]
+          dayList[day]
         } ${hours % 12}:${minutes} ${hours > 12 ? 'PM' : 'AM'}`}</button>
       </section>
     </div>
