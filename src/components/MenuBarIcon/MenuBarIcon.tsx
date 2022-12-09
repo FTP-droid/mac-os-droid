8c8563f08130401c4f92b3b5c07e6483d4975cd2
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-09 01:03:30 -0500

    Added initial test drop down menu for apple menubar btn

--- src/components/MenuBarIcon/MenuBarIcon.tsx
+++ src/components/MenuBarIcon/MenuBarIcon.tsx
@@ -38,6 +38,46 @@ function MenuBarIcon({
         className={styles[imgClass]}
         id={styles[imgId]}
       />
+      <div className={styles.dropdown}>
+        <button type="button" className={styles['dropdown-btn']}>
+          About this Mac
+        </button>
+        <hr />
+        <button type="button" className={styles['dropdown-btn']}>
+          System Preferences
+        </button>
+        <button type="button" className={styles['dropdown-btn']}>
+          Location
+        </button>
+        <button type="button" className={styles['dropdown-btn']}>
+          App Store
+        </button>
+        <hr />
+        <button type="button" className={styles['dropdown-btn']}>
+          Recent Items
+        </button>
+        <hr />
+        <button type="button" className={styles['dropdown-btn']}>
+          Force Quit...
+        </button>
+        <hr />
+        <button type="button" className={styles['dropdown-btn']}>
+          Sleep
+        </button>
+        <button type="button" className={styles['dropdown-btn']}>
+          Restart...
+        </button>
+        <button type="button" className={styles['dropdown-btn']}>
+          Shut Down...
+        </button>
+        <hr />
+        <button type="button" className={styles['dropdown-btn']}>
+          Lock Screen
+        </button>
+        <button type="button" className={styles['dropdown-btn']}>
+          Log Out
+        </button>
+      </div>
     </div>
   );
 }
