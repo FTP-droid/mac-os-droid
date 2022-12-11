c3d44bcd6073d76217576b4fcc0f93841e830325
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-10 23:47:51 -0500

    Added new file for storing menuBar icon dropdown values

---
+++ src/staticValues/menuBarIconValues.ts
@@ -0,0 +1,57 @@
+const appleDropDownValues = [
+  'About this Mac',
+  '1',
+  'System Preferences',
+  'Location',
+  'App Store',
+  '2',
+  'Recent Items',
+  '3',
+  'Force Quit...',
+  '4',
+  'Sleep',
+  'Restart...',
+  'Shut Down...',
+  '5',
+  'Lock Screen',
+  'Log Out',
+];
+
+const wifiDropDownValues = [
+  'Turn Wi-fi Off',
+  'j',
+  'BELL 123',
+  'ROGERS 153',
+  'DOG 10222',
+  'shade',
+  'SuShi',
+  'SCRUBS',
+  'q',
+  'Join Other Network...',
+  'Create Network...',
+  'Open Network Preferences...',
+];
+
+const batteryDropDownValues = [
+  'Power Source: Battery',
+  'o',
+  'No Apps Using Significant Energy',
+  'p',
+  'Show Percentage',
+  'Open Energy Saver Preferences...',
+];
+
+const dateDropDownValues = [
+  'View as Analog',
+  'View as Digital',
+  'r',
+  'Open Date & Time Preferences...',
+];
+
+const allIconDropDownValues = [
+  wifiDropDownValues,
+  batteryDropDownValues,
+  dateDropDownValues,
+];
+
+export { appleDropDownValues, allIconDropDownValues };
