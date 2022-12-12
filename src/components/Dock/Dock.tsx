e833a2cc3db865cc16790f48edda8f282c4af8aa
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-12 01:55:24 -0500

    Added more dock assets. Added tooltip for dock icons. Added dock icons.

--- src/components/Dock/Dock.tsx
+++ src/components/Dock/Dock.tsx
@@ -1,14 +1,25 @@
 import styles from './dock.module.css';
 import finder from '../../../assets/dockIcons/Finder.png';
+import safari from '../../../assets/dockIcons/Safari.png';
+import messages from '../../../assets/dockIcons/Messages.png';
 
 function Dock() {
+  const dockValues = [
+    { name: 'Finder', value: finder },
+    { name: 'Safari', value: safari },
+    { name: 'Messages', value: messages },
+  ];
+
   return (
-    <div className={styles.container}>
+    <article className={styles.container}>
       {/* CREATE LINK TO ICONS8 WEBSITE IN DESKTOP ICONS */}
-      <div>
-        <img src={finder} alt="" />
-      </div>
-    </div>
+      {dockValues.map((v) => (
+        <div className={styles['icon-container']} key={v.name}>
+          <img src={v.value} alt="" className={styles['icon-img']} />
+          <span className={styles['tool-tip']}>{v.name}</span>
+        </div>
+      ))}
+    </article>
   );
 }
 
