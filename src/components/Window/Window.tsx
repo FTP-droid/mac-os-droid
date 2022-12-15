8094f9a84b26be92dee48bc250dbb036e7c46af1
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-12-14 23:47:34 -0500

    Added draggable function to Window, bugs are present however

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,9 +1,33 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { DragEvent, useRef } from 'react';
 import styles from './window.module.css';
 
-function Window() {
+function Window({
+  setOffsetX,
+  setOffsetY,
+  styleLeft,
+  styleTop,
+}: {
+  setOffsetX: (x: number) => void;
+  setOffsetY: (y: number) => void;
+  styleLeft: string;
+  styleTop: string;
+}) {
+  const dummyRef = useRef<HTMLDivElement>(null);
+
+  const onDragStart = (e: DragEvent<HTMLDivElement>) => {
+    const rect = dummyRef?.current?.getBoundingClientRect();
+
+    setOffsetX(e.clientX - (rect ? rect.x : 0));
+    setOffsetY(e.clientY - (rect ? rect.y : 0));
+  };
+
   return (
-    <div className={styles.container} draggable="true">
+    <div
+      className={styles.container}
+      draggable="true"
+      onDragStart={onDragStart}
+      style={{ left: styleLeft, top: styleTop }}
+    >
       <div className={styles.header}>
         <div className={styles['button-container']}>
           <button type="button" className={styles.button} id={styles.close}>
--- src/components/Windows/Windows.tsx
+++ src/components/Windows/Windows.tsx
@@ -1,10 +1,41 @@
 import Window from 'components/Window/Window';
+import { DragEvent, useState } from 'react';
 import styles from './windows.module.css';
 
 function Windows() {
+  const [offsetX, setOffsetX] = useState(150);
+  const [offsetY, setOffsetY] = useState(150);
+  const [styleLeft, setStyleLeft] = useState('');
+  const [styleTop, setStyleTop] = useState('');
+
+  const onDragEnter = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  };
+
+  const onDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setStyleLeft(`${e.clientX - offsetX}px`);
+    setStyleTop(`${e.clientY - offsetY}px`);
+  };
+
   return (
-    <div className={styles.container}>
-      <Window />
+    <div
+      className={styles.container}
+      onDragEnter={onDragEnter}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+    >
+      <Window
+        setOffsetX={setOffsetX}
+        setOffsetY={setOffsetY}
+        styleLeft={styleLeft}
+        styleTop={styleTop}
+      />
     </div>
   );
 }
