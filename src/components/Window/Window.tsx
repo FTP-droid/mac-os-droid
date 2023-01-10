0175f6b4685989f76578f9941af9f90960612fa6
Alaister <7394050+FTP-droid@users.noreply.github.com>
2023-01-10 08:42:52 -0500

    Removed draggable function on Window component (too buggy)

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,39 +1,19 @@
 import { DragEvent, useRef } from 'react';
 import styles from './window.module.css';
 
-function Window({
-  setOffsetX,
-  setOffsetY,
-  styleLeft,
-  styleTop,
-}: {
-  setOffsetX: (x: number) => void;
-  setOffsetY: (y: number) => void;
-  styleLeft: string;
-  styleTop: string;
-}) {
-  const dummyRef = useRef<HTMLDivElement>(null);
-
-  const onDragStart = (e: DragEvent<HTMLDivElement>) => {
-    const rect = dummyRef?.current?.getBoundingClientRect();
-    setOffsetX(e.clientX - (rect ? rect.x : 0));
-    setOffsetY(e.clientY - (rect ? rect.y : 0));
-  };
+function Window() {
 
   return (
     <div
       className={styles.container}
-      draggable="true"
-      onDragStart={onDragStart}
-      style={{ left: styleLeft, top: styleTop }}
     >
       <div className={styles.header}>
         <div className={styles['button-container']}>
           <button type="button" className={styles.button} id={styles.close}>
-            <span className={styles['button-child']}>x</span>
+            x
           </button>
           <button type="button" className={styles.button} id={styles.minimize}>
-            <span className={styles['button-child']}>-</span>
+            -
           </button>
         </div>
       </div>
